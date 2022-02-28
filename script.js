7ae8b5c3d9d30654f92fe54fe2cb834ad63e5791
Ayon <ayonjodder177@gmail.com>
2022-03-01 03:29:06 +0600

    Explore by clicking added

--- script.js
+++ script.js
@@ -41,8 +41,12 @@ const button = document.getElementById('search-btn');
 button.addEventListener('click', () => {
     const searchField = document.getElementById('input');
     const searchText = searchField.value;
+    //Clearing previous search
     const cardParent = document.getElementById('card-parent');
+    const detailParent = document.getElementById('detail-parent');
     cardParent.innerHTML = '';
+    detailParent.innerHTML = '';
+    searchField.value = '';
     //Making the url dynamic
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
@@ -51,9 +55,72 @@ button.addEventListener('click', () => {
 });
 //Updating product by search result
 const updateBySearch = data => {
-    toDoData(data);
+    toDoData(data)
 }
 //Showing details by clicking Explore button
 const explore = slugData => {
-    
+    const url = `https://openapi.programming-hero.com/api/phone/${slugData}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => exploreInner(data))
+    const exploreInner = data => {
+        console.log(data.data)
+        const detailParent = document.getElementById('detail-parent');
+        detailParent.innerHTML = `
+    <div class="card mb-3 p-4">
+    <div class="row g-0">
+      <div class="col-md-4 d-flex align-items-center">
+        <img src="${data.data.image}" class="image-full mb-3 mb-md-0" rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${data.data.name}</h5>
+          <p class="card-text mb-0">Brand: ${data.data.brand}</p>
+          ${
+            (data => data.data.releaseDate ? `<p class="card-text"><small class="text-muted">${data.data.releaseDate}</small></p>` : `<p class="card-text"><small class="text-muted">Releasedate not found</small></p>`) (data)
+          }
+          
+          <hr>
+          <p class="card-text mb-0"><strong>Main Features:</strong></p>
+          <hr class="w-25 mt-0 mb-1">
+          ${
+            (data => data.data.mainFeatures.storage ? `<p class="card-text mb-0"><small class="text-muted"><strong>Storage:</strong> ${data.data.mainFeatures.storage}</small></p>` : `<p class="card-text"><small class="text-muted"> Not found</small></p>`) (data)
+          }
+          ${
+            (data => data.data.mainFeatures.memory ? `<p class="card-text mb-0"><small class="text-muted"><strong>Memory:</strong> ${data.data.mainFeatures.memory}</small></p>` : `<p class="card-text"><small class="text-muted"> Not found</small></p>`) (data)
+          }
+          ${
+            (data => data.data.mainFeatures.displaySize ? `<p class="card-text mb-0"><small class="text-muted"><strong>Display:</strong> ${data.data.mainFeatures.displaySize}</small></p>` : `<p class="card-text"><small class="text-muted"> Not found</small></p>`) (data)
+          }
+          ${
+            (data => data.data.mainFeatures.chipSet ? `<p class="card-text mb-0"><small class="text-muted"><strong>Chipset:</strong> ${data.data.mainFeatures.chipSet}</small></p>` : `<p class="card-text"><small class="text-muted">Chipset: Not found</small></p>`) (data)
+          }
+          ${
+            (data => data.data.mainFeatures.sensors ? `<p class="card-text mb-0"><small class="text-muted"><strong>Sensors:</strong> &nbsp;${data.data.mainFeatures.sensors}&nbsp;</small></p>` : `<p class="card-text"><small class="text-muted">Sensors: Not found</small></p>`) (data)
+          }
+          <hr>
+          <p class="card-text mb-0"><strong>Other Features:</strong></p>
+          <hr class="w-25 mt-0 mb-1">
+          ${
+            (data => data.data.others?.Bluetooth ? `<p class="card-text mb-0"><small class="text-muted"><strong>Bluetooth:</strong> &nbsp;${data.data.others.Bluetooth}</small></p>` : `<p class="card-text"><small class="text-muted">Bluetooth: Not Supported</small></p>`) (data)
+          }
+          ${
+            (data => data.data.others?.NFC ? `<p class="card-text mb-0"><small class="text-muted"><strong>NFC:</strong> &nbsp;${data.data.others.NFC}</small></p>` : `<p class="card-text"><small class="text-muted">NFC: Not Supported</small></p>`) (data)
+          }
+          ${
+            (data => data.data.others?.Radio ? `<p class="card-text mb-0"><small class="text-muted"><strong>Radio:</strong> &nbsp;${data.data.others.Radio}</small></p>` : `<p class="card-text"><small class="text-muted">Radio: Not Supported</small></p>`) (data)
+          }
+          ${
+            (data => data.data.others?.USB ? `<p class="card-text mb-0"><small class="text-muted"><strong>USB:</strong> &nbsp;${data.data.others.USB}</small></p>` : `<p class="card-text"><small class="text-muted">USB: No</small></p>`) (data)
+          }
+          ${
+            (data => data.data.others?.WLAN ? `<p class="card-text mb-0"><small class="text-muted"><strong>WALN:</strong> &nbsp;${data.data.others.WLAN}</small></p>` : `<p class="card-text"><small class="text-muted">WLAN: Not found</small></p>`) (data)
+          }
+
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+    }
 }
